aa9ec54a69cbda09bc6e827cd1d68a373a257b0c
Caio Zinsly <166133225+Caiozinsly@users.noreply.github.com>
2025-08-14 18:01:17 -0300

    Update useAuth.tsx

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -4,14 +4,13 @@ import { supabase } from "@/lib/supabaseClient";
 export const useAuth = () => {
   const [loading, setLoading] = useState(false);
 
-  // Função de login com Google, agora aceitando redirectTo
   const signInWithGoogle = async (options?: { redirectTo?: string }) => {
     try {
       setLoading(true);
       const { error } = await supabase.auth.signInWithOAuth({
         provider: "google",
         options: {
-          redirectTo: options?.redirectTo || window.location.origin, // fallback para o site atual
+          redirectTo: options?.redirectTo || window.location.origin,
         },
       });
       if (error) throw error;
